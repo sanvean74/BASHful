1767aaa5eee8df10ef4433c6581ceede9e9f79fb
sanvean74 <dlambertpdx@gmail.com>
2019-10-11 17:34:50 -0700

    get all matches test matching

--- e2e/__tests__/matches.test.js
+++ e2e/__tests__/matches.test.js
@@ -22,17 +22,7 @@ describe('matches api', () => {
     image: '/assets/images/testm.jpg'
   };
 
-  // function postMatch(match, user) {
-  //   return request
-  //     .post('/api/matches')
-  //     .set('Authorization', user.token)
-  //     .send(match)
-  //     .expect(200)
-  //     .then(({ body }) => body);
-  // }
-
   it('post a match for this user', () => {
-    console.log(user);
     return request
       .post('/api/matches')
       .set('Authorization', user.token)
@@ -41,8 +31,14 @@ describe('matches api', () => {
       .then(({ body }) => {
         expect(body).toMatchInlineSnapshot(
           {
-            _id: expect.any(String)
+            _id: expect.any(String),
+            location: [
+              {
+                _id: expect.any(String)
+              }
+            ]
           },
+
           `
           Object {
             "__v": 0,
@@ -54,7 +50,7 @@ describe('matches api', () => {
             "image": "/assets/images/testm.jpg",
             "location": Array [
               Object {
-                "_id": "5da119d705b47253ca9396af",
+                "_id": Any<String>,
                 "city": "Portland",
                 "state": "Oregon",
               },
@@ -65,89 +61,89 @@ describe('matches api', () => {
         );
       });
   });
+  function postMatch(match, user) {
+    return request
+      .post('/api/matches')
+      .set('Authorization', user.token)
+      .send(match)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+
+  it('gets a list of matches', () => {
+    const firstMatch = {
+      name: 'Light Yagami',
+      gender: 'male',
+      age: 19,
+      location: {
+        city: 'Portland',
+        state: 'Oregon'
+      },
+      image: '/assets/images/testm.jpg'
+    };
+    return Promise.all([
+      postMatch(firstMatch, user),
+      postMatch(
+        {
+          name: 'Tuxedo Mask',
+          gender: 'male',
+          age: 18,
+          location: {
+            city: 'Seattle',
+            state: 'Washington'
+          },
+          image: '/assets/images/testm.jpg'
+        },
+        user
+      ),
+
+      postMatch(
+        {
+          name: 'Sebastian Michaelis',
+          gender: 'male',
+          age: 99,
+          location: {
+            city: 'London',
+            state: 'England'
+          },
+          image: '/assets/images/testseb.jpg'
+        },
+        user
+      )
+    ])
+
+      .then(() => {
+        return request
+          .get('/api/matches')
+          .expect(200)
+          .set('Authorization', user.token)
+          .expect(200);
+      })
+      .then(({ body }) => {
+        expect(body.length).toBe(3);
+        expect(body[0]).toMatchInlineSnapshot(
+          {
+            _id: expect.any(String),
+            location: [
+              {
+                _id: expect.any(String)
+              }
+            ]
+          },
+
+          `
+          Object {
+            "_id": Any<String>,
+            "location": Array [
+              Object {
+                "_id": Any<String>,
+                "city": "Portland",
+                "state": "Oregon",
+              },
+            ],
+          }
+        `
+        );
+      });
+  });
 });
-// return request
-// .post('/api/matches')
-// .set('Authorization', user.token)
-// .send(match)
-// .expect(200)
-// .then(({ body }) => {
-//   expect(body).toMatchInlineSnapshot(
-//     {
-//       _id: expect.any(String),
-//       user: expect.any(String)
-//     },
-//   );
-// });
-// });
-
-// it('post a match', () => {
-//   return postMatch(match, user)
-//     .then(match => {
-//       expect(match).toEqual({
-//         _id: expect.any(String),
-//         __v: 0,
-//         ...match
-//       });
-//     });
-// });
-
-// it('gets a list of matches', () => {
-//   const firstMatch = {
-//     name: 'Light Yagami',
-//     gender: 'male',
-//     age: 19,
-//     location: {
-//       city: 'Portland',
-//       state: 'Oregon'
-//     },
-//     image: '/assets/images/testm.jpg'
-//   };
-//   return Promise.all([
-//     postMatch(firstMatch, user),
-//     postMatch(
-//       {
-//         name: 'Tuxedo Mask',
-//         gender: 'male',
-//         age: 18,
-//         location: {
-//           city: 'Seattle',
-//           state: 'Washington'
-//         },
-//         image: '/assets/images/testm.jpg'
-//       },
-//       user,
-//     ),
-
-//     postMatch(
-//       {
-//         name: 'Sebastian Michaelis',
-//         gender: 'male',
-//         age: 99,
-//         location: {
-//           city: 'London',
-//           state: 'England'
-//         },
-//         image: '/assets/images/testseb.jpg'
-//       },
-//       user,
-//     )
-//   ])
-
-//     .then(() => {
-//       return request
-//         .get('/api/matches')
-//         .expect(200)
-//         .set('Authorization', user.token)
-//         .expect(200);
-//     })
-//     .then(({ body }) => {
-//       expect(body.length).toBe(3);
-//       expect(body[0]).toEqual({
-//         _id: expect.any(String),
-//         name: firstMatch.name,
-//         age: firstMatch.age,
-//         location: firstMatch.location
-//       });
-//     });
-//});
