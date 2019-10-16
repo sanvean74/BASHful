0b5581d1267c6c5caf6e173cdf434f4884514d07
Angela Vawser <amvawser@gmail.com>
2019-10-16 10:20:20 -0700

    story setup

--- client/dateApp.js
+++ client/dateApp.js
@@ -78,7 +78,15 @@ const intermission = [
   {
     type: 'boolean',
     name: 'intermission',
-    message: 'Nice Choice! We have some questions before your date begins. Press enter to continue'
+    message: 'HAWT! We have some questions before your date begins. Press enter to continue'
+  }
+];
+
+const intermission2 = [
+  {
+    type: 'boolean',
+    name: 'intermission2',
+    message: 'Great job...now you\'re ready for your date! Press enter to continue'
   }
 ];
 
@@ -93,12 +101,12 @@ const dateQs = [
     type: 'input',
     name: 'venue',
     message: 'Where would you like to go on a date?',
-  },  
+  },
   {
     type: 'input',
     name: 'activity',
     message: 'What activity would you like to partake in? (ending in ing)',
-  },    
+  },
   {
     type: 'list',
     name: 'food',
@@ -109,44 +117,44 @@ const dateQs = [
     type: 'input',
     name: 'animal',
     message: 'What is your favorite animal? (plural)',
-  },   
+  },
   {
     type: 'input',
     name: 'color',
     message: 'What is your favorite color?',
-  },   
+  },
   {
     type: 'list',
     name: 'methodOfTravel',
     message: 'How do you like to get around when not driving?',
     choices: ['walking', 'busing', 'tandem unicyling', 'rafting', 'E-Scootering']
-  },   
+  },
   {
     type: 'input',
     name: 'place',
     message: 'Name a place you love to take people.',
-  },   
+  },
   {
     type: 'input',
     name: 'beverage',
-    message: 'What is your favorite beverage?',
-  },   
+    message: 'What is your beverage of choice?',
+  },
   {
     type: 'input',
     name: 'action',
     message: 'An action word ending in -ing.',
-  },   
+  },
   {
     type: 'list',
     name: 'restaurant',
     message: 'What restaurant/bar would you take a date to?',
     choices: ['Portland City Grill', 'Super Deluxe', 'Nicholas', 'Baby Doll Pizza', 'My Father\'s Place']
-  },   
+  },
   {
     type: 'input',
     name: 'clothing',
     message: 'What is your favorite article of clothing that you always wear on a first date?',
-  }, 
+  },
 ];
 
 let chosenThree;
@@ -225,8 +233,16 @@ const signupPrompt = () =>
           return newMatches(user);
         })
         .then(() => inquirer.prompt(intermission))
-        .then(() => inquirer.prompt(dateQs));
-    });
+        .then(() => inquirer.prompt(dateQs))
+        .then((answers, match, user) => {
+          inquirer.prompt(intermission2);
+          return request
+            .post(`${REQUEST_URL}/api/results`)
+            .set('Authorization', user.token)
+            .send({ result: `The witching hour fast approaches, I can hear the howls of city coyotes as I am ${answers.methodOfTravel} to meet my date at the Lone Fir Cemetery.I'm wearing the ${answers.color} feather in my hair to signal to my date that I have arrived. I met ${match.name} in front of a mausoleum in the NE corner of the cemetery. I think to myself, they sure we're brave to meet me here at this time of day, let's see if ${match.pronoun} has what it takes to keep up with me. We sat around the cemetery for a while talking and drinking some ${answers.beverage} I brought with me. After a while I suggested ${answers.activity}, which is met with enthusiasm by my new partner in crime. We headed off, plotting and laughing maniacally as we traveled to ${answers.place}. We spent several hours ${answers.activity} and worked up quite the appetite. Fortunately, by the time we decided we were done places started opening for breakfast. We headed off to ${answers.restaurant} for ${answers.food} where we spilled our guts to one another about our lives, hopes, and dreams. After spending these hours together we decided to meet up next Saturday night for ${answers.action} during the full moon at ${answers.venue}.  All in all, this turned out to be one of my best dates, if you can believe that.` })
+            .then(() => inquirer.prompt());
+        });
 
+    });
 
 module.exports = { signinPrompt, signupPrompt };
