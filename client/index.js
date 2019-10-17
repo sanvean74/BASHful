0e9439a63f465919da284f0e0e7973cc0b9644c0
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-17 15:50:45 -0700

    sound integrated

--- client/index.js
+++ client/index.js
@@ -1,3 +1,14 @@
 const startApp = require('./startApp');
+const { exec } = require('child_process');
 
 startApp();
+
+const audio = () => {
+  const cmd = exec('afplay assets/audio/berlin.mp3');
+  process.on('SIGINT', () => {
+    console.log('hello');
+    cmd.kill();
+  });
+};
+
+audio();
