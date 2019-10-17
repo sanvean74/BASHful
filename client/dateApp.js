a7aac81684ab8550a13b8cbb0365ea6d3c93b9ef
evanandrewson <evanandrewson@gmail.com>
2019-10-17 10:46:56 -0700

    finished about us

--- client/dateApp.js
+++ client/dateApp.js
@@ -135,39 +135,6 @@ const signupPrefs = [
   }
 ];
 
-const aboutUs = [
-  {
-    type: 'boolean',
-    name: 'introduction',
-    message: '\n WE ARE DEAD*ANT... (press enter to navigate) \n'
-  },
-  {
-    type: 'boolean',
-    name: 'Dylan',
-    message: 'Dylan: An agendered poly queer circus performer and punk/metal musician that enjoys spending time with their family in the forest and gardening. Their hobbies include: dance trapeze, hand-balancing, playing and making music, guitar, singing in choirs and screaming in bands. \n'
-  },
-  {
-    type: 'boolean',
-    name: 'Evan',
-    message: 'Evan: A full stack software developer. When he\'s not coding, he enjoys hiking, going to shows, and playing chess. \n'
-  },
-  {
-    type: 'boolean',
-    name: 'Angela',
-    message: 'Angela: Graphic Designer/Animator turned Software Developer. Enjoys costume making, cooking/baking, gardening, and horror/sci-fi flicks. \n'
-  },
-  {
-    type: 'boolean',
-    name: 'Donna',
-    message: 'Donna: Like a cat, but Vegan. Also likes chess. \n'
-  },
-  {
-    type: 'boolean',
-    name: 'Antonella',
-    message: 'Anonella: Loves cute and fluffy dogs and anime, dislikes cooked carrots. Played \'Dream Daddy\' twice. \n'
-  }
-];
-
 const intermission = [
   {
     type: 'boolean',
@@ -480,7 +447,7 @@ const signupPrompt = () =>
 
 const evanTest = ['Dylan: An agendered poly queer circus performer and punk/metal musician that enjoys spending time with their family in the forest and gardening. Their hobbies include: dance trapeze, hand-balancing, playing and making music, guitar, singing in choirs and screaming in bands. \n', 'Evan: A full stack software developer. When he\'s not coding, he enjoys hiking, going to shows, and playing chess. \n', 'Angela: Graphic Designer/Animator turned Software Developer. Enjoys costume making, cooking/baking, gardening, and horror/sci-fi flicks. \n', 'Donna: Like a cat, but Vegan. Also likes chess. \n', 'Anonella: Loves cute and fluffy dogs and anime, dislikes cooked carrots. Played \'Dream Daddy\' twice. \n'];
 
-const img = ['assets/images/ivyweb.jpeg', 'assets/images/testm.jpg', 'assets/images/testm.jpg', 'assets/images/testm.jpg', 'assets/images/testm.jpg'];
+const img = ['assets/images/dylan.png', 'assets/images/evan.png', 'assets/images/angela.png', 'assets/images/donna.png', 'assets/images/antonella.png'];
 
 const asciiLogo = 'assets/images/DeadAnt-Logo.png';
 
