2ced36e982a18dbabaf0a9222694d1d829e1a146
Angela Vawser <amvawser@gmail.com>
2019-10-17 17:07:02 -0700

    color added to about us

--- client/startApp.js
+++ client/startApp.js
@@ -5,7 +5,7 @@ const terminalImage = require('terminal-image');
 const startApp = [{
   type: 'list',
   name: 'start',
-  message: 'BASHful: A Terminal Date Simulator ©2019 \n',
+  message: 'BASHful: A Terminal Date Simulator © 1987 \n',
   choices: ['Sign In', 'Sign Up', 'Meet the Devs']
 }];
 
